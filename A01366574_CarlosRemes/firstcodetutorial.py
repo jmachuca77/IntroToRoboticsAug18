c6e7f5724583a5840b8b4d36d91794db713967ab
carlosremes00 <carlosremes00@gmail.com>
2018-09-03 16:29:28 -0400

    added code

--- A01366574_CarlosRemes/firstcodetutorial.py
+++ A01366574_CarlosRemes/firstcodetutorial.py
@@ -33,29 +33,40 @@ def arm_and_takeoff(vehicle, TargetAltitude):
 
 def executeMission(vehicle):
     print ("executing mission")
-    waypoint1 = LocationGlobalRelative(20.736905,-103.456639,20)
+    waypoint1 = LocationGlobalRelative(20.735371,-103.456121,20)
     vehicle.simple_goto(waypoint1)
     time.sleep(30)
 
-    waypoint2 = LocationGlobalRelative(20.736916,-103.455678,20)
+    waypoint2 = LocationGlobalRelative(20.735517,-103.457538,20)
     vehicle.simple_goto(waypoint2)
     time.sleep(30)
 
-    waypoint3 = LocationGlobalRelative(20.736007,-103.455654,20)
+    waypoint3 = LocationGlobalRelative(20.736438,-103.457430,20)
     vehicle.simple_goto(waypoint3)
     time.sleep(30)
 
-    waypoint4 = LocationGlobalRelative( 20.736007,-103.456639,20)
+    waypoint4 = LocationGlobalRelative(20.736303,-103.456085,20)
     vehicle.simple_goto(waypoint4)
     time.sleep(30)
+
+    vehicle.simple_goto(waypoint1)
+    time.sleep(30)
+
+    home = LocationGlobalRelative(20.735917,-103.456639,20)
+    vehicle.simple_goto(home)
+    time.sleep(30)
+
+     
 
 
 def main():
     vehicle = connect("udp:localhost:14551",wait_ready=True)
-
     arm_and_takeoff(vehicle,20)
+    vehicle.airspeed = 10
     executeMission(vehicle)
     vehicle.mode = VehicleMode("LAND")
+    battery = vehicle.battery
+    print (battery)
 
 
 if __name__ =="__main__":
